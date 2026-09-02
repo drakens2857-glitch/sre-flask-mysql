00ff1af0dfa268e50c02162161187e5335f62428
Nicolas Bello <draken.s2857@gmail.com>
2026-09-02 15:28:21 +0000

    feat: initial SRE architecture

---
+++ app/app.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, jsonify
+import pymysql
+
+app = Flask(__name__)
+
+
+def get_connection():
+    return pymysql.connect(
+        host=os.getenv("MYSQL_HOST", "servidor-bd"),
+        user=os.getenv("MYSQL_USER", "usuario"),
+        password=os.getenv("MYSQL_PASSWORD", "123456"),
+        database=os.getenv("MYSQL_DATABASE", "adso_db"),
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "ok",
+        "message": "API Flask funcionando"
+    }), 200
+
+
+@app.route("/health")
+def health():
+    connection = get_connection()
+    connection.close()
+
+    return jsonify({
+        "status": "ok",
+        "database": "connected"
+    }), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
